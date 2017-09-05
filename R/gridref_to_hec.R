7b55b246b5794114439dd33bcb5726a6b486f3df
wgaul <wgaul@hotmail.com>
2017-09-05 11:02:58 +0100

    Making the function work with vectors, not just individual character strings.

--- R/gridref_to_hec.R
+++ R/gridref_to_hec.R
@@ -10,29 +10,42 @@
 ###################################
 
 ## Grid Ref to hectad
-gridref_to_hec <- function(gr = "NULL") {
+gridref_to_hec <- function(gr = NULL) {
   # convert a grid ref (letter form) of any precision to hectad (3 characters)
   # ARGS: gr = character string giving the grid reference (e.g. "B1641", "B161415)
   # VALUE: a character string giving the 3-character hectad grid ref (e.g. "B14")
-  if(is.null(gr) || !is.character(gr)) {
+  # 
+  # TODO: eventually vectorize this using C++?
+  #
+  if(any(is.null(gr)) || any(!is.character(gr))) {
     stop("Provide a character string to 'gr'.")
   }
-
-  parts <- unlist(strsplit(gr, NULL))
-  nums <- grep("[[:digit:]]", parts, value = T)
-  letters <- paste0(grep("[[:alpha:]]", parts, value = T), collapse = "")
-  n <- length(nums)
   
-  if(nchar(gr)/2 == 0) {
-    stop("Original grid ref should have an even number of characters after the letter(s).")
+  result <- c()
+  
+  for(i in 1:length(gr)) { 
+    parts <- unlist(strsplit(gr[i], NULL))
+    nums <- grep("[[:digit:]]", parts, value = T)
+    letters <- paste0(grep("[[:alpha:]]", parts, value = T), collapse = "")
+    n <- length(nums)
+    
+    if(n %% 2 != 0) {
+      stop("There must be an even number of digits after the letter(s) in the original grid reference.")
+    }
+    
+    if(n == 2) {
+      hec <- gr[i]
+    }
+    
+    if(n > 2) {
+      half <- n/2
+      keep_n <- paste0(nums[c(1, half + 1)], collapse = "")
+      hec <- paste0(letters, keep_n)
+    } 
+    
+    result[i] <- hec
   }
-
-  if(n > 2) {
-    half <- n/2
-    keep_n <- paste0(nums[c(1, half + 1)], collapse = "")
-    hec <- paste0(letters, keep_n)
-  }
-  # return hectad code in the form of letter(s) followed by 1 digit each 
-  # for eastings and northings
-  hec 
+  # return a vector of hectad codes in the form of letter(s) followed by 1 
+  # digit each for eastings and northings
+  result 
 }
