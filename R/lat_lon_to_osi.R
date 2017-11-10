3c00e38d8151af939a212b0a1100e90e6e5f590a
wgaul <wgaul@hotmail.com>
2017-11-10 13:35:10 +0000

    Fixed bug that was producing very wrong easting and northing values.  Also added option to have results truncated to a specified precision (hectad or 1km block).

--- R/lat_lon_to_osi.R
+++ R/lat_lon_to_osi.R
@@ -3,7 +3,7 @@
 ##
 ## author: Willson Gaul
 ## created: 3 November 2017
-## last modified: 6 Nov 2017
+## last modified: 10 Nov 2017
 #########################################
 
 #' Convert WGS84 lat/long to OSI TM75 eastings/northings.
@@ -12,8 +12,16 @@
 #' grid.
 #'
 #' This function calls \code{\link[sp]{spTransform}} to convert
-#' latitude/longitude coordinates from WGS84 to the Irish OSI TM75 grid.  The
-#' main purpose of this function is to make this slightly more automatic for
+#' latitude/longitude coordinates from WGS84 or some other specified CRS to the
+#' Irish OSI TM75 grid.  The proj4string for the CRS of the original coordinates
+#' must be specified in \code{orig_crs}. (e.g. "+init=epsg:4326 for WGS 84).
+#'
+#' Use \code{precision_meters} to find the hectad or 1km block in which the
+#' original coordinates lie.  For example, \code{precision_meters = 1} gives
+#' results precise to 1 meter, and \code{precision_meters = 10000} gives
+#' the eastings/northings of the hectad in which the original coordinates lie.
+#'
+#' The main purpose of this function is to make this slightly more automatic for
 #' Willson's PhD work by providing the destination epsg:29903 code in the
 #' function.  The function also re-names the output coordinate fields to
 #' 'eastings' and 'northings'.
@@ -34,12 +42,11 @@
 #' @param x A data frame containing latitude and longitude coordinates.
 #' @param lat,lon Character string giving the names of the latitude and
 #'   longitude columns.
-#' @param orig_crs A character string giving the proj4string to pass to
-#'   \code{\link[sp]{CRS}}.
+#' @param orig_crs A character string giving the proj4string specifying the
+#'   coordinate reference system that the original lat/lon coordinates are in.
+#' @param precision_meters Integer giving the desired precision of results
 lat_lon_to_osi <- function(x, lat = "Latitude", lon = "Longitude",
-                           orig_crs = NULL) {
-
-
+                           orig_crs = NULL, precision_meters = 1) {
   if (!requireNamespace("sp", quietly = T)) {
     stop("Package sp needed for this function to work.  Please install it.",
          call. = F)
@@ -48,13 +55,20 @@ lat_lon_to_osi <- function(x, lat = "Latitude", lon = "Longitude",
     orig_crs <- sp::CRS(orig_crs)
   } else stop("Please specify a valid crs.")
 
-  sp::coordinates(x) <- x[, c(lat, lon)]
+  sp::coordinates(x) <- x[, c(lon, lat)]
   sp::proj4string(x) <- orig_crs
   conv_points <- sp::spTransform(x, "+init=epsg:29903")
   dimnames(conv_points@coords)[[2]][which(
     dimnames(conv_points@coords)[[2]] == "Latitude")] <- "northings"
   dimnames(conv_points@coords)[[2]][which(
     dimnames(conv_points@coords)[[2]] == "Longitude")] <- "eastings"
+
+  # truncate results to the desired precision
+  conv_points@coords[, 1] <- conv_points@coords[, 1] -
+    conv_points@coords[, 1]%%precision_meters
+  conv_points@coords[, 2] <- conv_points@coords[, 2] -
+    conv_points@coords[, 2]%%precision_meters
+
   conv_points <- as.data.frame(conv_points)
   conv_points
 }
